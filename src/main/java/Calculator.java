66357077d58db44f72ca747d1888564d0fe1f7a4
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-15 14:42:58 +0000

    first calculator test passing

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,14 @@
 public class Calculator {
+
+    private int value1;
+    private int value2;
+
+    public Calculator(int value1, int value2){
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    public int getValue1() {
+        return value1;
+    }
 }
