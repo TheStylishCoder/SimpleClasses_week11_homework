a0e304139e3271515dd3a2a916e335a593975a95
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-15 16:16:13 +0000

    first printer test passes

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,12 @@
 public class Printer {
+
+    private int numberOfSheets;
+
+    public Printer(int numberOfSheets){
+        this.numberOfSheets = numberOfSheets;
+    }
+
+    public int getNumberOfSheets() {
+        return numberOfSheets;
+    }
 }
